159bbcc27fa91356540b49bf72a32c278323b21d
saniyashaikh-hub <saniyashaikh03@eng.rizvi.edu.in>
2026-02-09 22:45:01 +0530

    Implement factorial calculation in main.py

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 # AIM: Design a Python program to compute 
 # the factorial of a given integer N.
-# Coder:
-# Date:
+# Coder:Saniya Shaikh
+# Date:30-01-2026
 
 print("--- Factorial Finder ---\n")
 
-
 # Write your code here
+N = int(input())
+
+if N < 0:
+    print(f"Factorial of {N} is Not Defined")
+else:
+    fact = 1
+    for x in range(1, N + 1):
+        fact = fact * x
+    print(f"Factorial of {N} is {fact}")
+
